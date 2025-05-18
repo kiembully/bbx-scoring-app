d15b8deb2ec499e5d75e380e324effdc6b85f64c
kiembully <kimklintonvale@gmail.com>
2025-05-18 14:11:06 +0800

    enhanced ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,11 @@ import { useState } from "react";
 type Team = "red" | "blue";
 
 const ScoreButtons = ({ onScore }: { onScore: (points: number) => void }) => (
-  <div className="grid grid-cols-4 bg-neutral-500">
-    <button onClick={() => onScore(1)} className="border p-6 w-full">SF</button>
-    <button onClick={() => onScore(2)} className="border p-6 w-full">BF</button>
-    <button onClick={() => onScore(3)} className="border p-6 w-full">XF</button>
-    <button onClick={() => onScore(2)} className="border p-6 w-full">OF</button>
+  <div className="grid grid-cols-4 gap-2 p-2 font-blackops text-xl text-white">
+    <button onClick={() => onScore(1)} className="w-full h-20 border rounded-lg p-2 box-shadow-lg bg-zinc-500 transition-all duration-100 active:bg-blue-600 active:shadow-inner active:ring-inset active:ring-blue-800">SF</button>
+    <button onClick={() => onScore(2)} className="w-full h-20 border rounded-lg p-2 box-shadow-lg bg-zinc-500 transition-all duration-100 active:bg-blue-600 active:shadow-inner active:ring-inset active:ring-blue-800">BF</button>
+    <button onClick={() => onScore(3)} className="w-full h-20 border rounded-lg p-2 box-shadow-lg bg-zinc-500 transition-all duration-100 active:bg-blue-600 active:shadow-inner active:ring-inset active:ring-blue-800">XF</button>
+    <button onClick={() => onScore(2)} className="w-full h-20 border rounded-lg p-2 box-shadow-lg bg-zinc-500 transition-all duration-100 active:bg-blue-600 active:shadow-inner active:ring-inset active:ring-blue-800">OF</button>
   </div>
 );
 
@@ -52,24 +52,27 @@ const Home = () => {
   return (
     <div className="flex items-center justify-center w-full">
       <MainLayout>
-        <div className="flex flex-col items-center justify-center gap-2 py-2 fixed top-0 left-0 right-0">
-          <div className="flex gap-4">
-            <button onClick={undoLast} className="bg-yellow-500 text-white px-2 py-1 rounded">Undo Last</button>
-            <button onClick={clearAll} className="bg-red-700 text-white px-2 py-1 rounded">Clear All</button>
+        <div className="flex flex-col items-center justify-center fixed left-0 right-0 bottom-[4px]">
+          <div className="flex flex-col gap-y-2 justify-between p-2">
+            <button onClick={undoLast} className="bg-yellow-500 text-white px-2 py-1 rounded w-20">Undo</button>
+            <button onClick={clearAll} className="bg-red-700 text-white px-2 py-1 rounded w-20">Clear</button>
           </div>
         </div>
 
         <div className="flex min-h-screen w-full">
           {/* Red team */}
           <div className="flex w-full flex-col min-h-screen bg-red-500">
-            <div className="h-full w-full flex items-center justify-center text-8xl text-white">{redScore}</div>
-            <ScoreButtons onScore={(points) => handleScore("red", points)} />
+            <div className="h-full w-full flex items-center justify-center text-[120px] font-blackops text-white">{redScore}</div>
+            <div className='pr-10 bg-neutral-600'>
+              <ScoreButtons onScore={(points) => handleScore("red", points)} />
+            </div>
           </div>
-
           {/* Blue team */}
           <div className="flex w-full flex-col min-h-screen bg-blue-500">
-            <div className="h-full w-full flex items-center justify-center text-8xl text-white">{blueScore}</div>
-            <ScoreButtons onScore={(points) => handleScore("blue", points)} />
+            <div className="h-full w-full flex items-center justify-center text-[120px] font-blackops text-white">{blueScore}</div>
+            <div className='pl-10 bg-neutral-600'>
+              <ScoreButtons onScore={(points) => handleScore("blue", points)} />
+            </div>
           </div>
         </div>
       </MainLayout>
